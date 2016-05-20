b577f812d1cd975e9d86736b5e0aeefaf6a807e4
Armaan Shah <armaanshah96@gmail.com>
2016-05-20 16:37:25 -0500

    added validation functions

--- bayes.py
+++ bayes.py
@@ -74,7 +74,36 @@ class Bayes_Classifier:
                   else:
                      self.negwordsfreq[w.lower()]+= 1
                      self.total_negative += 1
-                     
+      
+   def train2(self, lFileList):   
+      """Trains the Naive Bayes Sentiment Classifier."""
+
+
+      #flag will be -1 for a negative word
+      #flag will be 1 for a positive word
+      for sFilename in lFileList:
+         if sFilename[7] == '5':
+            for w in self.tokenize(self.loadFile('movies_reviews/' + sFilename)):
+               if w.lower() not in punctuation_stopwords:
+                  if w.lower() not in self.poswordsfreq:
+                     self.poswordsfreq[w.lower()] = 1
+                     self.total_positive = 1
+                  else:
+                     self.poswordsfreq[w.lower()]+= 1
+                     self.total_positive += 1
+
+         elif sFilename[7] == '1':
+            for w in self.tokenize(self.loadFile('movies_reviews/' + sFilename)):
+               if w.lower() not in punctuation_stopwords:
+                  print sFilename
+                  if w.lower() not in self.negwordsfreq:
+                     self.negwordsfreq[w.lower()] = 1
+                     self.total_negative = 1
+                  else:
+                     self.negwordsfreq[w.lower()]+= 1
+                     self.total_negative += 1
+   
+
    def total_positive_words(self):
       counter = 0
       for key in self.poswordsfreq:
@@ -88,6 +117,61 @@ class Bayes_Classifier:
          counter = counter + self.negwordsfreq[key]
       self.total_negative = counter
       return self.total_negative
+
+   def cross_validation(self, nfold):
+      all_files = self.loop_files()
+      portion = len(all_files)/nfold #finding a tenth (or n-th) of the data to keep as testing
+
+
+
+      #still need to account for the remainder when divided by 10
+
+      for i in range(nfold):
+         if i == nfold-1:
+            test = all_files[i*portion:] # test files from the last set, ranging from starting position of the portion to the end of the set
+                                         # this absorbs the remainder
+            train = all_files[:i*portion] # train files all files before the test set
+
+         elif i == 0:
+            test = all_files[i*portion:(i+1)*portion] #setting the test portion from next set of length portion
+            train = all_files[portion+1:] # setting train set to be everything after the first portion test set
+
+         else:
+            test = all_files[i*portion:(i+1)*portion] #setting the test portion from next set of length portion
+            train = all_files[:i*portion] + all_files[(i+1)*portion:]
+
+         self.validate(test,train)
+
+
+###REALIZED THAT WE PROBABLY DONT NEED THIS
+   def separate_pos_neg_files(self, test, train):
+      pos_train = []
+      neg_train = []
+      neutral_train = []
+
+      ## Separate files into positive negative and neutral train sets
+
+      for i in train:
+         if self.classify(self.loadFile(i)) == 'positive':
+            pos_train.append(i)
+
+         elif self.classify(self.loadFile(i)) = 'negative':
+            neg_train.append(i)
+
+         else:
+            neutral_train.append(i)
+
+      validate(self, test, pos_train, neg_train, neutral_train) #validate test set against train sets
+
+   def validate(self, test, train):
+      self.train2(train)
+      sentiments =[]
+      for i in test:
+         file_text = self.loadFile(i)
+         sentiments.append(self.classify(file_text))
+
+      # compare the sentiments of those files to what we already had them classified as!!
+
 
    def classify(self, sText):
       """Given a target string sText, this function returns the most likely document
