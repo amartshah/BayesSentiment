d68295942eda968cb359fd50379d53fb32344eff
Sonia Nigam <sonianigam@curbside.in>
2016-05-22 19:53:47 -0500

    comments

--- bayes.py
+++ bayes.py
@@ -5,6 +5,7 @@
 # All group members were present and contributing during all work on this project.
 
 import math, os, pickle, re
+#stopword list to filter punctuation
 punctuation_stopwords = [" ", ".", '"', ",", "?", "!", "/", "'", "-", "_", ";", ":", "&","<",">", ',"', '",', ")", "(", "://", "/"]
 
 class Bayes_Classifier:
@@ -14,25 +15,32 @@ class Bayes_Classifier:
       cache of a trained classifier has been stored, it loads this cache.  Otherwise, 
       the system will proceed through training.  After running this method, the classifier 
       is ready to classify input text."""
+	  #dictionaries that capture the frequency of words associated to positive texts and negative texts
       self.poswordsfreq = dict()
       self.negwordsfreq = dict()
+	  #total words in the corresponding negative and positive dicts
       self.total_negative = 0
       self.total_positive = 0
+	  #populate an array of all the movie files
       if os.path.isfile('positive_words.txt') == False:
          lFileList = []
          for fFileObj in os.walk("movies_reviews/"):
             lFileList = fFileObj[2]
             break
+         #train the system based on all of the movie files
          self.train(lFileList)
+         #save the corresponding frequency dicts in txt files
          self.save(self.poswordsfreq,"positive_words.txt")
          self.save(self.negwordsfreq,"negative_words.txt")
       else:
+         #load the preexisting frequency dictionaries and total counts
          self.poswordsfreq = self.load("positive_words.txt")
          self.negwordsfreq = self.load("negative_words.txt") 
          self.total_negative = self.total_negative_words()
          self.total_positive = self.total_positive_words()
 
    def loop_files(self):
+      """this method populates an array of all the movie files"""
       lFileList = []
       for fFileObj in os.walk("movies_reviews/"):
          lFileList = fFileObj[2]
@@ -42,14 +50,18 @@ class Bayes_Classifier:
       return lFileList
 
    def total_positive_words(self):
+      """this method counts all of the words in the positive frequency dict"""
       counter = 0
+      #increment counter for each key
       for key in self.poswordsfreq:
          counter = counter + self.poswordsfreq[key]
       self.total_positive = counter
       return self.total_positive
 
    def total_negative_words(self):
+      '''this method counts all of the words in the negative frequency dict'''
       counter = 0
+      #increment the counter for each key
       for key in self.negwordsfreq:
          counter = counter + self.negwordsfreq[key]
       self.total_negative = counter
@@ -57,170 +69,108 @@ class Bayes_Classifier:
 
    def train(self, lFileList):   
       """Trains the Naive Bayes Sentiment Classifier."""
-      # lFileList = self.loop_files()
-
-
-      #flag will be -1 for a negative word
-      #flag will be 1 for a positive word
+      #iterate through each files
       for sFilename in lFileList:
+         #checks positive files
          if sFilename[7] == '5':
+            #tokenize file name
             for w in self.tokenize(self.loadFile('movies_reviews/' + sFilename)):
+               #take the lowercase and make sure it is not punctuation  
                if w.lower() not in punctuation_stopwords:
+                  #if the word is not already in the dict
                   if w.lower() not in self.poswordsfreq:
                      self.poswordsfreq[w.lower()] = 1
                      self.total_positive = 1
+                     #if the word is already in the dict
                   else:
                      self.poswordsfreq[w.lower()]+= 1
                      self.total_positive += 1
 
+         #checks negative files
          elif sFilename[7] == '1':
+            #tokenize the file name
             for w in self.tokenize(self.loadFile('movies_reviews/' + sFilename)):
+               #take the lowercase and make sure it is not punctuation
                if w.lower() not in punctuation_stopwords:
-                  # print sFilename
+                  # if the word is not in the dict
                   if w.lower() not in self.negwordsfreq:
                      self.negwordsfreq[w.lower()] = 1
                      self.total_negative = 1
+                     #if the word is already in the dict
                   else:
                      self.negwordsfreq[w.lower()]+= 1
                      self.total_negative += 1
       
-   def train2(self, lFileList):   
-      """Trains the Naive Bayes Sentiment Classifier."""
 
-
-      #flag will be -1 for a negative word
-      #flag will be 1 for a positive word
-      for sFilename in lFileList:
-         if sFilename[7] == '5':
-            for w in self.tokenize(self.loadFile('movies_reviews/' + sFilename)):
-               if w.lower() not in punctuation_stopwords:
-                  if w.lower() not in self.poswordsfreq:
-                     self.poswordsfreq[w.lower()] = 1
-                     self.total_positive = 1
-                  else:
-                     self.poswordsfreq[w.lower()]+= 1
-                     self.total_positive += 1
-
-         elif sFilename[7] == '1':
-            for w in self.tokenize(self.loadFile('movies_reviews/' + sFilename)):
-               if w.lower() not in punctuation_stopwords:
-                  # print sFilename
-                  if w.lower() not in self.negwordsfreq:
-                     self.negwordsfreq[w.lower()] = 1
-                     self.total_negative = 1
-                  else:
-                     self.negwordsfreq[w.lower()]+= 1
-                     self.total_negative += 1
 
    def cross_validation(self):
+      '''executes cross_validation test and returns three heuristics: recall, precision, and f measure'''
+      #populate an array of all of the files
       all_files = self.loop_files()
       # print "length" + str(len(all_files))
       portion = len(all_files)/10 #finding a tenth (or n-th) of the data to keep as testing
       #portion is the size of each faction
+      #initialize varibles to track starting index, aggreate false positive, false negative, true positive, and true negative
       starting_index = 0
       false_positive = 0
       false_negative = 0
       true_positive = 0
       true_negative = 0
 
+      #iterates ten times
       for i in range(0,10):
+         #initiate an empty test list
          test_set = []
+         #copy all files to initial training set list
          training_set = copy.deepcopy(all_files)
-         print len(all_files)
 
+         #iterate through the next tenth of the files
          for j in range(0, portion):
-            print starting_index
-            print j
-            print len(all_files)
+            #populate the test set
             test_set.append(all_files[starting_index + j])
 
          # print len(test_set)
-          
+         #increment the starting index to adjust for the next tenth
          starting_index = starting_index + portion
+         #delete the test files from the aggregate file training list
          for file in test_set:
             training_set.remove(file)
-
+         #train the data based on the 9/10 files
          self.train(training_set)
-
+         #iterate through the test files and detect accuracy
          for sFilename in test_set:
             file = self.loadFile('movies_reviews/' + sFilename)
             verdict = self.classify(file)
-
+            #handles negative cases
             if verdict == 'negative':
+               #increment true negative
                if sFilename[7] == '1':
                   true_negative = true_negative + 1
                else:
+                  #increment false negative
                   false_negative = false_negative + 1
 
             if verdict == 'positive':
+               #increment true positive
                if sFilename[7] == '1':
                   true_positive = true_positive + 1
                else:
+                  #increment false positive
                   false_positive = false_positive + 1
 
             # print false_positive, false_negative, true_positive, true_negative
-
+      #calculate precision, recall, and f-measure heuristics based on formulas and aggregate data
       precision = float(true_positive)/float(true_positive+false_positive)
       recall = float(true_positive)/float(true_positive+ false_negative)
-	
       f_measure = float(2*precision*recall)/(precision+recall)
 
       print "precision: " + str(precision)
       print "recall: " + str(recall)
       print "f_measure: " + str(f_measure)
+      #return results
       return precision, recall, f_measure
 
 
-
-
-              
-
-#         if i == nfold-1:
-#            test = all_files[i*portion:] # test files from the last set, ranging from starting position of the portion to the end of the set
-#                                         # this absorbs the remainder
-#            train = all_files[:i*portion] # train files all files before the test set
-#
-#         elif i == 0:
-#            test = all_files[i*portion:(i+1)*portion] #setting the test portion from next set of length portion
-#            train = all_files[portion+1:] # setting train set to be everything after the first portion test set
-#
-#         else:
-#            test = all_files[i*portion:(i+1)*portion] #setting the test portion from next set of length portion
-#            train = all_files[:i*portion] + all_files[(i+1)*portion:]
-#
-#         self.validate(test,train)
-
-
-
-####REALIZED THAT WE PROBABLY DONT NEED THIS
-#   def separate_pos_neg_files(self, test, train):
-#      pos_train = []
-#      neg_train = []
-#      neutral_train = []
-#
-#      ## Separate files into positive negative and neutral train sets
-#
-#      for i in train:
-#         if self.classify(self.loadFile(i)) == 'positive':
-#            pos_train.append(i)
-#
-#         elif self.classify(self.loadFile(i)) == 'negative':
-#            neg_train.append(i)
-#
-#         else:
-#            neutral_train.append(i)
-#
-#      validate(self, test, pos_train, neg_train, neutral_train) #validate test set against train sets
-#
-#   def validate(self, test, train):
-#      self.train2(train)
-#      sentiments =[]
-#      for i in test:
-#         file_text = self.loadFile(i)
-#         sentiments.append(self.classify(file_text))
-#
-#      # compare the sentiments of those files to what we already had them classified as!!
-#
 
    def classify(self, sText):
       """Given a target string sText, this function returns the most likely document
@@ -231,32 +181,43 @@ class Bayes_Classifier:
       neg_cond_prob = math.log10(2735.0/(2735.0 + 11129.0))
       #number of negative files is 2735
       #number of positive files is 11129
-
+      #iterate through each word in the tokenized text
       for w in self.tokenize(sText):
+         #if the word is not punctuation and already in positive frequency dict
          if w.lower() not in punctuation_stopwords and w.lower() in self.poswordsfreq:
+            #find probability of word based on relative frequency
             pos_prob_word = float(self.poswordsfreq[w.lower()] + 1)/self.total_positive
+            #determine positive conditional probablity
             pos_cond_prob = pos_cond_prob + math.log10(pos_prob_word)
          else:
+            #determine positive conditional probablity for words not present in the system
             pos_cond_prob += math.log10(1.0/self.total_positive)
 
       for w in self.tokenize(sText):
          # print self.negwordsfreq
          # print sText
+         #if the word is not punctuation and already in negative frequency dict
          if w.lower() not in punctuation_stopwords and w.lower() in self.negwordsfreq:
+            #find probability of word based on relative frequency
             neg_prob_word = float(self.negwordsfreq[w.lower()] + 1)/self.total_negative
+            #determine negative conditional probablity
             neg_cond_prob = neg_cond_prob + math.log10(neg_prob_word)
          else:
+            #determine negative conditional probablity for words not present in the system
             neg_cond_prob += math.log10(1.0/self.total_negative)
 
 
       # print pos_cond_prob
       # print neg_cond_prob
+      #determine neutral cases
       if (abs(pos_cond_prob-neg_cond_prob) < .5):
          # print "neutral"
          return "neutral"
+      #determine positive cases
       elif pos_cond_prob > neg_cond_prob:
          # print "positive"
          return "positive"
+      #determine negative cases
       elif neg_cond_prob > pos_cond_prob:
          # print "negative"
          return "negative"
@@ -308,6 +269,3 @@ class Bayes_Classifier:
 
       return lTokens
 
-
-
-b = Bayes_Classifier()
--- bayesbest.py
+++ bayesbest.py
@@ -5,6 +5,7 @@
 # All group members were present and contributing during all work on this project.
 
 import math, os, pickle, re, nltk,copy
+#stopword list to filter punctuation
 punctuation_stopwords = [" ", ".", '"', ",", "?", "!", "/", "'", "-", "_", ";", ":", "&","<",">", ',"', '",', ")", "(", "://", "/"]
 
 class Bayes_Classifier:
@@ -14,25 +15,32 @@ class Bayes_Classifier:
       cache of a trained classifier has been stored, it loads this cache.  Otherwise, 
       the system will proceed through training.  After running this method, the classifier 
       is ready to classify input text."""
+      #dictionaries that capture the frequency of words associated to positive texts and negative texts
       self.poswordsfreq = dict()
       self.negwordsfreq = dict()
+     #total words in the corresponding negative and positive dicts
       self.total_negative = 0
       self.total_positive = 0
+     #populate an array of all the movie files
       if os.path.isfile('positive_words_best.txt') == False:
          lFileList = []
          for fFileObj in os.walk("movies_reviews/"):
             lFileList = fFileObj[2]
             break
+         #train the system based on all of the movie files
          self.train(lFileList)
+         #save the corresponding frequency dicts in txt files
          self.save(self.poswordsfreq,"positive_words_best.txt")
          self.save(self.negwordsfreq,"negative_words_best.txt")
       else:
+         #load the preexisting frequency dictionaries and total counts
          self.poswordsfreq = self.load("positive_words_best.txt")
          self.negwordsfreq = self.load("negative_words_best.txt") 
          self.total_negative = self.total_negative_words()
          self.total_positive = self.total_positive_words()
 
    def loop_files(self):
+      """this method populates an array of all the movie files"""
       lFileList = []
       for fFileObj in os.walk("movies_reviews/"):
          lFileList = fFileObj[2]
@@ -46,57 +54,78 @@ class Bayes_Classifier:
       # lFileList = self.loop_files()
       #flag will be -1 for a negative word
       #flag will be 1 for a positive word
+      #iterate through each files
       for sFilename in lFileList:
+         #checks positive files
          if sFilename[7] == '5':
+            #tokenize file name
             file = self.loadFile('movies_reviews/' + sFilename)
             tokens = self.tokenize(file)
+            #extract bigrams 
             bigrams_text = nltk.bigrams(tokens)
-            
+            #iterate through bigrams
             for (w,x) in bigrams_text:
+               #check to see if the bigrams are in punctuation
                if w.lower() not in punctuation_stopwords and x.lower() not in punctuation_stopwords:
+                  #if the bigrams are not already in the dict, increment
                   if (w.lower(), x.lower()) not in self.poswordsfreq:
                      self.poswordsfreq[(w.lower(), x.lower())] = 1
                      self.total_positive = 1
                   else:
+                     #if the bigrams are in the dict, increment
                      self.poswordsfreq[(w.lower(), x.lower())]+= 1
                      self.total_positive += 1
-
+         #checks negative files
          elif sFilename[7] == '1':
+            #tokenize file name
             file = self.loadFile('movies_reviews/' + sFilename)
             #print file
             tokens = self.tokenize(file)
+            #extract bigrams
             bigrams_text = nltk.bigrams(tokens)
             #print bigrams_text
+            #iterate through the bigrams
             for (w,x) in bigrams_text:
+               #check and make sure the bigrams are not punctuation
                if w.lower() not in punctuation_stopwords and x.lower() not in punctuation_stopwords:
                   #print sFilename
+                  #if the bigram is not already in the dict, increment
                   if (w.lower(), x.lower()) not in self.negwordsfreq:
                      self.negwordsfreq[(w.lower(), x.lower())] = 1
                      self.total_negative = 1
                   else:
+                     #if the bigram is already in the dict, increment
                      self.negwordsfreq[(w.lower(), x.lower())]+= 1
                      self.total_negative += 1
       
 
    def total_positive_words(self):
+      """this method counts all of the words in the positive frequency dict"""
       counter = 0
+      #increment counter for each key
       for key in self.poswordsfreq:
          counter = counter + self.poswordsfreq[key]
       self.total_positive = counter
       return self.total_positive
 
    def total_negative_words(self):
+      '''this method counts all of the words in the negative frequency dict'''
       counter = 0
+      #increment the counter for each key
       for key in self.negwordsfreq:
          counter = counter + self.negwordsfreq[key]
       self.total_negative = counter
       return self.total_negative
 
    def cross_validation(self):
+      '''executes cross_validation test and returns three heuristics: recall, precision, and f measure'''
+      #populate an array of all of the files
+
       all_files = self.loop_files()
       #print "length" + str(len(all_files))
       portion = len(all_files)/10 #finding a tenth (or n-th) of the data to keep as testing
       #portion is the size of each faction
+      #initialize varibles to track starting index, aggreate false positive, false negative, true positive, and true negative
       starting_index = 0
       false_positive = 0
       false_negative = 0
@@ -104,40 +133,51 @@ class Bayes_Classifier:
       true_negative = 0
       print portion
 
+      #iterates ten times
       for i in range(0,10):
+         #initiate an empty test list
          test_set = []
+         #copy all files to initial training set list
          training_set = copy.deepcopy(all_files)
          print len(all_files)
 
+         #iterate through the next tenth of the files
          for j in range(0, portion):
+            #populate the test set
             print starting_index
             print j
             print len(all_files)
             test_set.append(all_files[starting_index + j])
-         
+         #increment the starting index to adjust for the next tenth
          starting_index = starting_index + portion
+         #delete the test files from the aggregate file training list
          for file in test_set:
             training_set.remove(file)
-
+         #train the data based on the 9/10 files
          self.train(training_set)
-
+         #iterate through the test files and detect accuracy
          for sFilename in test_set:
             file = self.loadFile('movies_reviews/' + sFilename)
             verdict = self.classify(file)
 
+            #handles negative cases
             if verdict == 'negative':
+               #increment true negative
                if sFilename[7] == '1':
                   true_negative = true_negative + 1
                else:
+                  #increment false negative
                   false_negative = false_negative + 1
 
             if verdict == 'positive':
+               #increment true positive
                if sFilename[7] == '1':
                   true_positive = true_positive + 1
                else:
+                  #increment false positive
                   false_positive = false_positive + 1
 
-
+      #calculate precision, recall, and f-measure heuristics based on formulas and aggregate data
       precision = float(true_positive)/float(true_positive+false_positive)
       recall = float(true_positive)/float(true_positive+ false_negative)
    
@@ -146,6 +186,7 @@ class Bayes_Classifier:
       print "precision: " + str(precision)
       print "recall: " + str(recall)
       print "f_measure: " + str(f_measure)
+      #return results
       return precision, recall, f_measure
 
    def classify(self, sText):
@@ -157,30 +198,44 @@ class Bayes_Classifier:
       neg_cond_prob = math.log10(2735.0/(2735.0 + 11129.0))
       #number of negative files is 2735
       #number of positive files is 11129
+      #iterate through each word in the tokenized text
+
+      #extract bigrams
       file_words = self.tokenize(sText)
       bigrams_text = nltk.bigrams(file_words)
     
+      #iterate through bigrams
       for w, v in bigrams_text:
+         #if the word is not punctuation and already in positive frequency dict
          if w.lower() not in punctuation_stopwords and v.lower() not in punctuation_stopwords and (w.lower(), v.lower()) in self.poswordsfreq:
+            #find probability of word based on relative frequency
             pos_prob_word = float(self.poswordsfreq[(w.lower(), v.lower())] + 1)/self.total_positive
+            #determine positive conditional probablity   
             pos_cond_prob = pos_cond_prob + math.log10(pos_prob_word)
          else:
+            #determine positive conditional probablity for words not present in the system
             pos_cond_prob += math.log10(1.0/self.total_positive)
 
       for w,v in bigrams_text:
+         #if the word is not punctuation and already in negative frequency dict
          if w.lower() not in punctuation_stopwords and v.lower() not in self.negwordsfreq and (w.lower(), v.lower()) in self.negwordsfreq:
+            #if the word is not punctuation and already in negative frequency dict
             neg_prob_word = float(self.negwordsfreq[(w.lower(), v.lower())] + 1)/self.total_negative
+            #determine negative conditional probablity
             neg_cond_prob = neg_cond_prob + math.log10(neg_prob_word)
          else:
+            #determine negative conditional probablity for words not present in the system
             neg_cond_prob += math.log10(1.0/self.total_negative)
       
-
+      #determine neutral cases
       if (abs(pos_cond_prob-neg_cond_prob) < .5):
          # print "neutral"
          return "neutral"
+      #determine positive cases
       elif pos_cond_prob > neg_cond_prob:
          # print "positive"
          return "positive"
+      #determine negative cases
       elif neg_cond_prob > pos_cond_prob:
          # print "negative"
          return "negative"
